7c2b96351016a41fc1706724581b5d5d7292967a
duongng2911 <duongng2911@gmail.com>
2023-12-20 09:50:24 +0700

    update chat components

--- frontend/src/components/chat.js
+++ frontend/src/components/chat.js
@@ -78,7 +78,12 @@ export default function Chat() {
             />
           </svg>
           <div className="prompt-container">
-            <input type="text"></input>
+            <textarea
+              placeholder="Enter a prompt here"
+              rows="1"
+              cols="100"
+              id="textarea"
+            />
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="17"
